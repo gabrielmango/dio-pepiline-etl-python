bc196d7a3cbe97ee6607be1dfa9ac873bf41792d
Gabriel Mango <gabrielhmango@gmail.com>
2023-08-21 15:54:18 -0300

    Etapa de transformação dos dados

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+from translate import Translator
 
 res = requests.get('https://hp-api.onrender.com/api/spells')
 
@@ -9,3 +9,17 @@ if res.status_code == 200:
 else:
     print(f'Erro: {res.status_code}')
     dados = None
+
+translator = Translator(to_lang="pt")
+
+dados_traduzidos = []
+
+for dado in dados:
+    descricao = translator.translate(dado['description'])
+    dados_traduzidos.append(
+        [
+            dado['id'],
+            dado['name'],
+            descricao
+        ]
+    )
